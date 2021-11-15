f0164afc27aa22fff9e372dfeddf49e3357df589
Pablo Casals <pcasals76@gmail.com>
2021-11-14 21:12:36 -0300

    mini proyecto terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import html2canvas from "html2canvas";
 
 function App() {
+  const [line1, setLine1] = useState("");
+  const [line2, setLine2] = useState("");
+  const [image, setImage] = useState();
+  const onChangeLine1 = function(e){
+    setLine1(e.target.value)
+  }
+  const onChangeLine2 = function(e){
+    setLine2(e.target.value)
+  }
+  const onChangeImage = function(e){
+    setImage(e.target.value)
+  }
+  const onClickExport = function(){
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      var img = canvas.toDataURL("image/png");
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+  });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Select pícker memes */}
+      <select onChange={onChangeImage}>
+        <option value="fire">Flame house</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History Channel</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select>{" "}
+      <br />
+
+      {/* Input text first line */}
+      <input onChange={onChangeLine1} type="text" placeholder="upper line" /> <br />
+      
+      {/* Input text last line */}
+      <input onChange={onChangeLine2} type="text" placeholder="last line" /> <br />
+      
+      {/* Export button */}
+      <button onClick={onClickExport}>Export</button>
+      
+      {/* Finished meme */}
+      <div className="meme" id="meme">
+        <span>{line1}</span> <br />
+        <span>{line2}</span>
+        <img src={`/memes/${image}.jpg`} alt="" />
+      </div>
+    
     </div>
   );
 }
